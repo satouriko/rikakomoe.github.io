7a7344bfa027e3f09ac2523e1314df81b7d53ce0
LI JIAHAO <lijiahao99131@gmail.com>
2018-01-11 01:32:52 +0800

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,8 @@
 import "../../node_modules/font-awesome/scss/font-awesome.scss";
+import "../../node_modules/unsemantic/assets/sass/unsemantic-grid-responsive.scss";
+
+import "../scss/header.scss";
+import "../scss/identities.scss";
+
+import "../scss/chem-elements.scss";
 import "../scss/index.scss"
